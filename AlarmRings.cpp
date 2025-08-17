d8725fe3b7ff3cae4d8f3d7c7d2e198ca8995848
Mohanapriya2828 <mohanapriya.jayabalan@psiog.com>
2025-08-18 01:05:14 +0530

    Third commit

--- AlarmRings.cpp
+++ AlarmRings.cpp
@@ -2,6 +2,21 @@
 #include <string>
 using namespace std;
 
+char getYesNoInput(string prompt) {
+    char choice;
+    while (true) {
+        cout << prompt;
+        cin >> choice;
+        choice = tolower(choice);
+
+        if (choice == 'y' || choice == 'n') {
+            return choice;
+        } else {
+            cout << "Invalid input! Please enter y/n.\n";
+        }
+    }
+}
+
 void alarmProcess();
 void checkingTime();
 void snooze();
@@ -16,11 +31,9 @@ int main() {
 
 void alarmProcess() {
     cout << "Alarm rings...\n";
-    char choice;
-    cout << "Are you ready to wake up? (y/n): ";
-    cin >> choice;
+    char choice=getYesNoInput("Are you ready to wake up? (y/n): ");
 
-    if (tolower(choice) == 'y') {
+    if (choice == 'y') {
         cout << "You are ready to wake up!\n";
         return; 
     } else {
@@ -29,11 +42,11 @@ void alarmProcess() {
 }
 
 void checkingTime() {
-    char choice;
-    cout << "Will you be late if you don't wake up now? (y/n): ";
-    cin >> choice;
+    char choice=getYesNoInput("Will you be late? (y/n): ");
 
-    if (tolower(choice) == 'y') {
+    
+
+    if (choice == 'y') {
         cout << "You will be late! Better wake up now.\n";
         return; 
     } else {
