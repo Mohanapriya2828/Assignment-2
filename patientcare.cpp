9ef13c017b8021d21c2b085f97ade557fde5572d
Mohanapriya2828 <mohanapriya.jayabalan@psiog.com>
2025-08-18 00:10:15 +0530

    Second commit

--- patientcare.cpp
+++ patientcare.cpp
@@ -35,3 +35,38 @@ int main() {
     return 0;
 }
 
+
+void patientRegistration() {
+    char choice;
+    cout << "Is patient already registered? (y/n): ";
+    cin >> choice;
+
+    if (tolower(choice) == 'y') {
+        cout << "Patient already registered. Skipping registration...\n";
+    } else {
+        cout << "Registering new patient...\n";
+    }
+}
+
+bool nurseAvailability() {
+    char choice;
+    cout << "Is nurse available? (y/n): ";
+    cin >> choice;
+    return (tolower(choice) == 'y');
+}
+
+bool doctorAvailability() {
+    char choice;
+    cout << "Is doctor available? (y/n): ";
+    cin >> choice;
+    return (tolower(choice) == 'y');
+}
+
+void recordHealthCondition() {
+    cout << "Recording patient health condition...\n";
+}
+
+void assignDoctor() {
+    cout << "Assigning patient to doctor...\n";
+}
+
