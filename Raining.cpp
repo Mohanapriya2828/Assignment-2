1d338c6ef8ee59fa4f10077ca6f7278cb63fb897
Mohanapriya2828 <mohanapriya.jayabalan@psiog.com>
2025-08-17 23:30:08 +0530

    Second commit

--- Raining.cpp
+++ Raining.cpp
@@ -2,6 +2,31 @@
 #include <cctype>   
 using namespace std;
 
+bool CheckRaining() {
+    char rain;
+    cout << "\nIs it raining? (y/n): ";
+    cin >> rain;
+    rain = tolower(rain);
+
+    if (rain == 'y') return true;
+    if (rain == 'n') return false;
+
+    cout << "Invalid input! Please enter y/n." << endl;
+    return CheckRaining();
+}
+
+bool CheckUmbrella() {
+    char umbrella;
+    cout << "Do you have an umbrella? (y/n): ";
+    cin >> umbrella;
+    umbrella = tolower(umbrella); 
+
+    if (umbrella == 'y') return true;
+    if (umbrella == 'n') return false;
+
+    cout << "Invalid input! Please enter y/n." << endl;
+    return CheckUmbrella(); 
+}
 
 int main() {
     if (CheckRaining()) {           
