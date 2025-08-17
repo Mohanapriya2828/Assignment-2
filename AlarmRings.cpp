a239b0f79a729941872a0d6ab41b680993137c1d
Mohanapriya2828 <mohanapriya.jayabalan@psiog.com>
2025-08-18 00:54:53 +0530

    Second commit

--- AlarmRings.cpp
+++ AlarmRings.cpp
@@ -28,3 +28,27 @@ void alarmProcess() {
     }
 }
 
+void checkingTime() {
+    char choice;
+    cout << "Will you be late if you don't wake up now? (y/n): ";
+    cin >> choice;
+
+    if (tolower(choice) == 'y') {
+        cout << "You will be late! Better wake up now.\n";
+        return; 
+    } else {
+        snooze();
+    }
+}
+
+void snooze() {
+    cout << "Snooze activated. Wait 5 minutes...\n";
+    cout << "After 5 minutes...\n";
+    alarmProcess(); 
+}
+
+void leavingBed() {
+    cout << "Leaving bed...\n";
+    cout << "Turn off alarm.\n";
+    
+}
