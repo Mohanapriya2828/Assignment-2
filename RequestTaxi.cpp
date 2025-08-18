1bc3c977cc81df49f53f941c303e58803b42ffc2
Mohanapriya2828 <mohanapriya.jayabalan@psiog.com>
2025-08-18 23:34:36 +0530

    Second commit

--- RequestTaxi.cpp
+++ RequestTaxi.cpp
@@ -21,6 +21,55 @@ void checkTime() {
     }
 }
 
+string findingTaxi() {
+    cout << "Finding taxi within 10 km of pickup location...\n";
+    createCSV(availableFile, "TaxiID,Pickup,Drop");
+
+    {
+        ifstream checkFile(availableFile);
+        checkFile.seekg(0, ios::end);
+        if (checkFile.tellg() < 30) {
+            ofstream file(availableFile, ios::app);
+            file << "TX001,Available,Available\n";
+            file.close();
+        }
+    }
+
+    bool taxiFound = false;
+    string assignedTaxiID = "TX001";
+
+    while (!taxiFound) {
+        char ans;
+        cout << "Taxi found? (y/n): ";
+        cin >> ans;
+
+        if (ans == 'y' || ans == 'Y') {
+            taxiFound = true;
+            {
+                ifstream inFile(availableFile);
+                ofstream outFile("temp.csv");
+                string header, line;
+                getline(inFile, header);
+                outFile << header << "\n";
+                while (getline(inFile, line)) {
+                    if (line.find(assignedTaxiID) != string::npos) {
+                        outFile << assignedTaxiID << ",Picked,Dropped\n";
+                    } else {
+                        outFile << line << "\n";
+                    }
+                }
+            }
+            this_thread::sleep_for(chrono::milliseconds(100));
+            fs::remove(availableFile);
+            fs::rename("temp.csv", availableFile);
+        } else {
+            cout << "No taxi yet. Wait 1 minute and check again...\n";
+            this_thread::sleep_for(chrono::seconds(1));
+        }
+    }
+    return assignedTaxiID;
+}
+
 void rideHistory(const string& methodStr, const string& taxiID) {
     assignTaxi(methodStr, taxiID);
     cout << "Ride Completed and Stored in History.\n";
