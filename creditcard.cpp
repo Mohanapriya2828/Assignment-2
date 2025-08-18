63ffe90d34361c00102149615664b88e84463f6c
Mohanapriya2828 <mohanapriya.jayabalan@psiog.com>
2025-08-18 16:00:41 +0530

    Second commit

--- creditcard.cpp
+++ creditcard.cpp
@@ -14,6 +14,52 @@ char getYesNo(string prompt) {
     }
 }
 
+void processPayment() {
+    cout << "Order placed by client." << endl;
+
+    char paymentVerified, cardValid, fundsAvailable, accountValid;
+
+    
+    paymentVerified = getYesNo("Is payment verified by card network?");
+    
+    while (paymentVerified == 'y') {
+       
+        cardValid = getYesNo("Is card valid?");
+        
+        if (cardValid == 'y') {
+            
+            fundsAvailable = getYesNo("Are funds available?");
+            
+            if (fundsAvailable == 'y') {
+ 
+                accountValid = getYesNo("Is account valid?");
+                if (accountValid == 'y') {
+                    cout << "Transaction complete." << endl;
+                    cout << "Card holder receives statement at end of cycle." << endl;
+                    return;
+                } else {
+                    
+                    cout << "Account not valid, returning to payment verification." << endl;
+                    paymentVerified = getYesNo("Is payment verified by card network?");
+                    continue; 
+                }
+            } else {
+                
+                cout << "Funds not available, returning to payment verification." << endl;
+                paymentVerified = getYesNo("Is payment verified by card network?");
+                continue; 
+            }
+        } else {
+            
+            cout << "Card not valid, returning to payment verification." << endl;
+            paymentVerified = getYesNo("Is payment verified by card network?");
+            continue; 
+        }
+    }
+
+    
+    cout << "Payment not verified by card network. Transaction returned." << endl;
+}
 
 int main() {
     processPayment(); 
